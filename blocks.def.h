c6d45420628ff6eb84e7b69ca748c7289824b54a
2nd1rst <2nd1rst@gmail.com>
2023-02-20 18:03:02 -0500

    not sure change

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Bat:", "awk '{print $1 \"%\"}' /sys/class/power_supply/BAT0/capacity",		10,		0},
 	{"Wifi:", " cat /sys/class/net/wlan0/operstate",					10,		0},
-	{"", "awk '/32 host/{print a}{a=$2}' /proc/net/fib_trie | awk 'NR==4{print}'",		10,		0},
+	{"", "ip route | awk '/default/{print $9}'",						20,		0},
 	{"Mem:", "free -h | awk '/^Mem/{print $3}' | sed s/i//g",				10,		0},
 	{"/:", "df -h | awk '/root/{print $4}'",						60,		0},
 	{"", "date '+%b %d (%a) %I:%M%p'",							5,		0},
