429f0c3ec47d2b1a021701344f44b7cfd0a0b8a1
2nd1rst <2nd1rst@gmail.com>
2023-02-19 23:54:29 -0500

    added disk size

--- blocks.def.h
+++ blocks.def.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
 	{"Wifi:", " cat /sys/class/net/wlan0/operstate",					10,		0},
 	{"", "awk '/32 host/{print a}{a=$2}' /proc/net/fib_trie | awk 'NR==4{print}'",		10,		0},
 	{"Mem:", "free -h | awk '/^Mem/{print $3}' | sed s/i//g",				10,		0},
+	{"/:", "df -h | awk '/root/{print $4}'",						60,		0},
 	{"", "date '+%b %d (%a) %I:%M%p'",							5,		0},
 };
 
