5d661b158c79a648819a9003cf4c757c279dc58d
2nd1rst <2nd1rst@gmail.com>
2023-02-19 23:49:02 -0500

    added battery,wifi,ip,mem,date, removed unused line from dwmblocks.c

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"Bat:", "awk '{print $1 \"%\"}' /sys/class/power_supply/BAT0/capacity",		10,		0},
+	{"Wifi:", " cat /sys/class/net/wlan0/operstate",					10,		0},
+	{"", "awk '/32 host/{print a}{a=$2}' /proc/net/fib_trie | awk 'NR==4{print}'",		10,		0},
+	{"Mem:", "free -h | awk '/^Mem/{print $3}' | sed s/i//g",				10,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",							5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
